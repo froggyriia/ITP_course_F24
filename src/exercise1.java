e1a74573bcc74067f0950b88f7fb416f50d2adbb
froggyriia <nira.osamu@yandex.ru>
2024-11-12 15:01:02 +0300

    first commit (addition)

--- src/exercise1.java
+++ src/exercise1.java
@@ -27,6 +27,7 @@ public class exercise1 {
     public static void main(String[] args) {
         Level level = Level.MEDIUM;
         System.out.println(level.getLevelCode());
+        System.out.println("i'm experimenting");
 
     }
 }
